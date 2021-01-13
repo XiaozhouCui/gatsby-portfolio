038567bc9e64522c81cdf839bf8dc878cb6c2625
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-13 17:49:22 +1000

    Rendered Projects Page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ const Project = ({ description, title, github, stack, url, image, index }) => {
     <article className="project">
       <Image fluid={image.childImageSharp.fluid} className="project-img" />
       <div className="project-info">
-        <span className="project-number">0{index + 1}</span>
+        <span className="project-number">0{index + 1}.</span>
         <h3>{title}</h3>
         <p className="project-desc">{description}</p>
         <div className="project-stack">
