760e4ea95b1bfb6e20534b564747b0f91bc2b2c6
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-14 10:04:15 +1000

    Added blog page

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -2,10 +2,41 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({
+  data: {
+    allStrapiBlogs: { nodes: blogs },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="blog" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        slug
+        date(formatString: "DD-MMMM-YYYY")
+        id
+        title
+        category
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
