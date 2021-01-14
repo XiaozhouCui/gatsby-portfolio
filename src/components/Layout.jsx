79b5e6843b679e01d20f26bca3d1b3c417db222e
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-14 15:55:52 +1000

    Toggle Sidebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 // import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleSideBar = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <>
-      <Navbar />
+      <Navbar toggleSideBar={toggleSideBar} />
+      <Sidebar toggleSideBar={toggleSideBar} isOpen={isOpen} />
       {children}
       <Footer />
     </>
